btcsuite/btcwallet

commit 4b64adf52f873a4ec5237d3fb16b5252ddef3846

Refactor address pool code and automatically resync accounts from seed
The old address pool code was faulty in that it could not scale to
more accounts than the default account and did address index alignment
using an actual address instead of an index. This updates the address
pools to be generalized so that in the future they can be multiaccount.
Address indexes for an account are now stored in the meta bucket of the
address manager database. The logic for the address pool has been
greatly simplified.

Accounts are now restored when the wallet is restored from seed. New
logic similar to the logarithmic scan for addresses in the default
account has been added. On start up, the wallet will also rescan and
check for new address usage in all of its used accounts. On first start
up, the wallet must be unlocked so that accounts can be added if
necessary. New logic prompts the user for the password.

A new flag, --promptpass, has been added. This will cause the wallet
to prompt the user for the password on startup, leaving the wallet
unlocked indefinitely.

wallet/loader.go
@@ -52,6 +52,7 @@ type Loader struct {
 	stakeOptions  *StakeOptions
 	autoRepair    bool
 	unsafeMainNet bool
+	promptPass    bool
 }
 
 type StakeOptions struct {
@@ -66,12 +67,13 @@ type StakeOptions struct {
 }
 
 // NewLoader constructs a Loader.
-func NewLoader(chainParams *chaincfg.Params, dbDirPath string, stakeOptions *StakeOptions, autoRepair bool, unsafeMainNet bool) *Loader {
+func NewLoader(chainParams *chaincfg.Params, dbDirPath string, stakeOptions *StakeOptions, autoRepair bool, unsafeMainNet bool, promptPass bool) *Loader {
 	return &Loader{
 		chainParams:  chainParams,
 		dbDirPath:    dbDirPath,
 		stakeOptions: stakeOptions,
 		autoRepair:   autoRepair,
+		promptPass:   promptPass,
 	}
 }
 
@@ -139,19 +141,9 @@ func (l *Loader) CreateNewWallet(pubPassphrase, privPassphrase, seed []byte) (*W
 		return nil, err
 	}
 
-	// Open the newly-created wallet.
-	so := l.stakeOptions
-	w, err := Open(db, pubPassphrase, nil, so.VoteBits, so.StakeMiningEnabled,
-		so.BalanceToMaintain, so.AddressReuse, so.RollbackTest,
-		so.PruneTickets, so.TicketAddress, so.TicketMaxPrice, l.autoRepair,
-		l.chainParams)
-	if err != nil {
-		return nil, err
-	}
-	w.Start()
-
-	l.onLoaded(w, db)
-	return w, nil
+	l.onLoaded(nil, db)
+	db.Close()
+	return nil, nil
 }
 
 var errNoConsole = errors.New("db upgrade requires console access for additional input")
@@ -163,7 +155,7 @@ func noConsole() ([]byte, error) {
 // OpenExistingWallet opens the wallet from the loader's wallet database path
 // and the public passphrase.  If the loader is being called by a context where
 // standard input prompts may be used during wallet upgrades, setting
-// canConsolePrompt will enables these prompts.
+// canConsolePrompt will enable these prompts.
 func (l *Loader) OpenExistingWallet(pubPassphrase []byte, canConsolePrompt bool) (*Wallet, error) {
 	defer l.mu.Unlock()
 	l.mu.Lock()
@@ -201,10 +193,11 @@ func (l *Loader) OpenExistingWallet(pubPassphrase []byte, canConsolePrompt bool)
 	w, err := Open(db, pubPassphrase, cbs, so.VoteBits, so.StakeMiningEnabled,
 		so.BalanceToMaintain, so.AddressReuse, so.RollbackTest,
 		so.PruneTickets, so.TicketAddress, so.TicketMaxPrice, l.autoRepair,
-		l.chainParams)
+		l.promptPass, l.chainParams)
 	if err != nil {
 		return nil, err
 	}
+
 	w.Start()
 
 	l.onLoaded(w, db)

config.go
@@ -38,6 +38,7 @@ const (
 	defaultTicketMaxPrice    = 50.0
 	defaultAutomaticRepair   = false
 	defaultUnsafeMainNet     = false
+	defaultPromptPass        = false
 
 	walletDbName = "wallet.db"
 )
@@ -74,6 +75,7 @@ type config struct {
 
 	// Wallet options
 	WalletPass        string  `long:"walletpass" default-mask:"-" description:"The public wallet password -- Only required if the wallet was created with one"`
+	PromptPass        bool    `long:"promptpass" description:"The private wallet password is prompted for at start up, so the wallet starts unlocked without a time limit"`
 	DisallowFree      bool    `long:"disallowfree" description:"Force transactions to always include a fee"`
 	EnableStakeMining bool    `long:"enablestakemining" description:"Enable stake mining"`
 	VoteBits          uint16  `long:"votebits" description:"Set your stake mining votebits to value (default: 0xFFFF)"`
@@ -229,7 +231,14 @@ func parseAndSetDebugLevels(debugLevel string) error {
 // The above results in dcrwallet functioning properly without any config
 // settings while still allowing the user to override settings with config files
 // and command line options.  Command line options always take precedence.
+// The bool returned indicates whether or not the wallet was recreated from a
+// seed and needs to perform the initial resync. The []byte is the private
+// passphrase required to do the sync for this special case.
 func loadConfig() (*config, []string, error) {
+	loadConfigError := func(err error) (*config, []string, error) {
+		return nil, nil, err
+	}
+
 	// Default config.
 	cfg := config{
 		DebugLevel:             defaultLogLevel,
@@ -237,6 +246,7 @@ func loadConfig() (*config, []string, error) {
 		DataDir:                defaultDataDir,
 		LogDir:                 defaultLogDir,
 		WalletPass:             wallet.InsecurePubPassphrase,
+		PromptPass:             defaultPromptPass,
 		RPCKey:                 defaultRPCKeyFile,
 		RPCCert:                defaultRPCCertFile,
 		LegacyRPCMaxClients:    defaultRPCMaxClients,
@@ -256,7 +266,7 @@ func loadConfig() (*config, []string, error) {
 	exists, err := cfgutil.FileExists(defaultConfigFilename)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 	if exists {
 		cfg.ConfigFile = defaultConfigFile
@@ -271,7 +281,7 @@ func loadConfig() (*config, []string, error) {
 		if e, ok := err.(*flags.Error); !ok || e.Type != flags.ErrHelp {
 			preParser.WriteHelp(os.Stderr)
 		}
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 
 	// Show the version and exit if the version flag was specified.
@@ -292,7 +302,7 @@ func loadConfig() (*config, []string, error) {
 		if _, ok := err.(*os.PathError); !ok {
 			fmt.Fprintln(os.Stderr, err)
 			parser.WriteHelp(os.Stderr)
-			return nil, nil, err
+			return loadConfigError(err)
 		}
 		configFileError = err
 	}
@@ -303,7 +313,7 @@ func loadConfig() (*config, []string, error) {
 		if e, ok := err.(*flags.Error); !ok || e.Type != flags.ErrHelp {
 			parser.WriteHelp(os.Stderr)
 		}
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 
 	// Warn about missing config file after the final command line parse
@@ -342,7 +352,7 @@ func loadConfig() (*config, []string, error) {
 		err := fmt.Errorf(str, "loadConfig")
 		fmt.Fprintln(os.Stderr, err)
 		parser.WriteHelp(os.Stderr)
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 
 	// Append the network type to the log directory so it is "namespaced"
@@ -365,7 +375,7 @@ func loadConfig() (*config, []string, error) {
 		err := fmt.Errorf("%s: %v", "loadConfig", err.Error())
 		fmt.Fprintln(os.Stderr, err)
 		parser.WriteHelp(os.Stderr)
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 
 	// Exit if you try to use a simulation wallet with a standard
@@ -400,13 +410,13 @@ func loadConfig() (*config, []string, error) {
 		err := fmt.Errorf("The flags --create and --createtemp can not " +
 			"be specified together. Use --help for more information.")
 		fmt.Fprintln(os.Stderr, err)
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 
 	dbFileExists, err := cfgutil.FileExists(dbPath)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 
 	if cfg.CreateTemp {
@@ -422,14 +432,14 @@ func loadConfig() (*config, []string, error) {
 		// Ensure the data directory for the network exists.
 		if err := checkCreateDir(netDir); err != nil {
 			fmt.Fprintln(os.Stderr, err)
-			return nil, nil, err
+			return loadConfigError(err)
 		}
 
 		if !tempWalletExists {
 			// Perform the initial wallet creation wizard.
 			if err := createSimulationWallet(&cfg); err != nil {
 				fmt.Fprintln(os.Stderr, "Unable to create wallet:", err)
-				return nil, nil, err
+				return loadConfigError(err)
 			}
 		}
 	} else if cfg.Create {
@@ -439,25 +449,25 @@ func loadConfig() (*config, []string, error) {
 			err := fmt.Errorf("The wallet database file `%v` "+
 				"already exists.", dbPath)
 			fmt.Fprintln(os.Stderr, err)
-			return nil, nil, err
+			return loadConfigError(err)
 		}
 
 		// Ensure the data directory for the network exists.
 		if err := checkCreateDir(netDir); err != nil {
 			fmt.Fprintln(os.Stderr, err)
-			return nil, nil, err
+			return loadConfigError(err)
 		}
 
 		// Perform the initial wallet creation wizard.
 		if !cfg.CreateWatchingOnly {
-			if err := createWallet(&cfg); err != nil {
+			if err = createWallet(&cfg); err != nil {
 				fmt.Fprintln(os.Stderr, "Unable to create wallet:", err)
-				return nil, nil, err
+				return loadConfigError(err)
 			}
 		} else if cfg.CreateWatchingOnly {
-			if err := createWatchingOnlyWallet(&cfg); err != nil {
+			if err = createWatchingOnlyWallet(&cfg); err != nil {
 				fmt.Fprintln(os.Stderr, "Unable to create wallet:", err)
-				return nil, nil, err
+				return loadConfigError(err)
 			}
 		}
 
@@ -468,7 +478,7 @@ func loadConfig() (*config, []string, error) {
 		keystoreExists, err := cfgutil.FileExists(keystorePath)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
-			return nil, nil, err
+			return loadConfigError(err)
 		}
 		if !keystoreExists {
 			err = fmt.Errorf("The wallet does not exist.  Run with the " +
@@ -478,7 +488,7 @@ func loadConfig() (*config, []string, error) {
 				"--create option to import it.")
 		}
 		fmt.Fprintln(os.Stderr, err)
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 
 	if len(cfg.TicketAddress) != 0 {
@@ -488,7 +498,7 @@ func loadConfig() (*config, []string, error) {
 			err := fmt.Errorf(str, funcName, cfg.TicketAddress, err)
 			fmt.Fprintln(os.Stderr, err)
 			fmt.Fprintln(os.Stderr, usageMessage)
-			return nil, nil, err
+			return loadConfigError(err)
 		}
 	}
 
@@ -502,7 +512,7 @@ func loadConfig() (*config, []string, error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr,
 			"Invalid rpcconnect network address: %v\n", err)
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 
 	localhostListeners := map[string]struct{}{
@@ -512,7 +522,7 @@ func loadConfig() (*config, []string, error) {
 	}
 	RPCHost, _, err := net.SplitHostPort(cfg.RPCConnect)
 	if err != nil {
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 	if cfg.DisableClientTLS {
 		if _, ok := localhostListeners[RPCHost]; !ok {
@@ -522,7 +532,7 @@ func loadConfig() (*config, []string, error) {
 			err := fmt.Errorf(str, funcName, cfg.RPCConnect)
 			fmt.Fprintln(os.Stderr, err)
 			fmt.Fprintln(os.Stderr, usageMessage)
-			return nil, nil, err
+			return loadConfigError(err)
 		}
 	} else {
 		// If CAFile is unset, choose either the copy or local dcrd cert.
@@ -534,7 +544,7 @@ func loadConfig() (*config, []string, error) {
 			certExists, err := cfgutil.FileExists(cfg.CAFile)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
-				return nil, nil, err
+				return loadConfigError(err)
 			}
 			if !certExists {
 				if _, ok := localhostListeners[RPCHost]; ok {
@@ -542,7 +552,7 @@ func loadConfig() (*config, []string, error) {
 						dcrdHomedirCAFile)
 					if err != nil {
 						fmt.Fprintln(os.Stderr, err)
-						return nil, nil, err
+						return loadConfigError(err)
 					}
 					if dcrdCertExists {
 						cfg.CAFile = dcrdHomedirCAFile
@@ -560,7 +570,7 @@ func loadConfig() (*config, []string, error) {
 	if len(cfg.ExperimentalRPCListeners) == 0 && len(cfg.LegacyRPCListeners) == 0 {
 		addrs, err := net.LookupHost("localhost")
 		if err != nil {
-			return nil, nil, err
+			return loadConfigError(err)
 		}
 		cfg.LegacyRPCListeners = make([]string, 0, len(addrs))
 		for _, addr := range addrs {
@@ -576,14 +586,14 @@ func loadConfig() (*config, []string, error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr,
 			"Invalid network address in legacy RPC listeners: %v\n", err)
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 	cfg.ExperimentalRPCListeners, err = cfgutil.NormalizeAddresses(
 		cfg.ExperimentalRPCListeners, activeNet.RPCServerPort)
 	if err != nil {
 		fmt.Fprintf(os.Stderr,
 			"Invalid network address in RPC listeners: %v\n", err)
-		return nil, nil, err
+		return loadConfigError(err)
 	}
 
 	// Both RPC servers may not listen on the same interface/port.
@@ -599,7 +609,7 @@ func loadConfig() (*config, []string, error) {
 					"used as a listener address for both "+
 					"RPC servers", addr)
 				fmt.Fprintln(os.Stderr, err)
-				return nil, nil, err
+				return loadConfigError(err)
 			}
 		}
 	}
@@ -617,7 +627,7 @@ func loadConfig() (*config, []string, error) {
 				err := fmt.Errorf(str, funcName, addr, err)
 				fmt.Fprintln(os.Stderr, err)
 				fmt.Fprintln(os.Stderr, usageMessage)
-				return nil, nil, err
+				return loadConfigError(err)
 			}
 			if _, ok := localhostListeners[host]; !ok {
 				str := "%s: the --noservertls option may not be used " +
@@ -626,7 +636,7 @@ func loadConfig() (*config, []string, error) {
 				err := fmt.Errorf(str, funcName, addr)
 				fmt.Fprintln(os.Stderr, err)
 				fmt.Fprintln(os.Stderr, usageMessage)
-				return nil, nil, err
+				return loadConfigError(err)
 			}
 		}
 	}

dcrwallet.go
@@ -94,7 +94,7 @@ func walletMain() error {
 		TicketAddress:      cfg.TicketAddress,
 		TicketMaxPrice:     cfg.TicketMaxPrice,
 	}
-	loader := wallet.NewLoader(activeNet.Params, dbDir, stakeOptions, cfg.AutomaticRepair, cfg.UnsafeMainNet)
+	loader := wallet.NewLoader(activeNet.Params, dbDir, stakeOptions, cfg.AutomaticRepair, cfg.UnsafeMainNet, cfg.PromptPass)
 
 	// Create and start HTTP server to serve wallet client connections.
 	// This will be updated with the wallet and chain server RPC client

internal/prompt/prompt.go
@@ -113,10 +113,10 @@ func promptListBool(reader *bufio.Reader, prefix string, defaultEntry string) (b
 	return response == "yes" || response == "y", nil
 }
 
-// promptPass prompts the user for a passphrase with the given prefix.  The
+// PromptPass prompts the user for a passphrase with the given prefix.  The
 // function will ask the user to confirm the passphrase and will repeat the
 // prompts until they enter a matching response.
-func promptPass(reader *bufio.Reader, prefix string, confirm bool) ([]byte, error) {
+func PromptPass(reader *bufio.Reader, prefix string, confirm bool) ([]byte, error) {
 	// Prompt the user until they enter a passphrase.
 	prompt := fmt.Sprintf("%s: ", prefix)
 	for {
@@ -161,7 +161,7 @@ func PrivatePass(reader *bufio.Reader, legacyKeyStore *keystore.Store) ([]byte, 
 	// When there is not an existing legacy wallet, simply prompt the user
 	// for a new private passphase and return it.
 	if legacyKeyStore == nil {
-		return promptPass(reader, "Enter the private "+
+		return PromptPass(reader, "Enter the private "+
 			"passphrase for your new wallet", true)
 	}
 
@@ -172,7 +172,7 @@ func PrivatePass(reader *bufio.Reader, legacyKeyStore *keystore.Store) ([]byte, 
 		"your existing legacy wallet will be imported into the new " +
 		"wallet format.")
 	for {
-		privPass, err := promptPass(reader, "Enter the private "+
+		privPass, err := PromptPass(reader, "Enter the private "+
 			"passphrase for your existing wallet", false)
 		if err != nil {
 			return nil, err
@@ -228,7 +228,7 @@ func PublicPass(reader *bufio.Reader, privPass []byte,
 	}
 
 	for {
-		pubPass, err = promptPass(reader, "Enter the public "+
+		pubPass, err = PromptPass(reader, "Enter the public "+
 			"passphrase for your new wallet", true)
 		if err != nil {
 			return nil, err
@@ -261,7 +261,8 @@ func PublicPass(reader *bufio.Reader, privPass []byte,
 // seed.  When the user answers no, a seed will be generated and displayed to
 // the user along with prompting them for confirmation.  When the user answers
 // yes, a the user is prompted for it.  All prompts are repeated until the user
-// enters a valid response.
+// enters a valid response. The bool returned indicates if the wallet was
+// restored from a given seed or not.
 func Seed(reader *bufio.Reader) ([]byte, error) {
 	// Ascertain the wallet generation seed.
 	useUserSeed, err := promptListBool(reader, "Do you have an "+

dangu327/dcrwallet

commit 3a5fc27b2703e8421277052764355016e7c042a0

Open and return wallet from CreateNewWallet.
This fixes a dcrwallet-specific bug introduced by
4b64adf52f873a4ec5237d3fb16b5252ddef3846 which removed the call to
wallet.Open from wallet.Loader.CreateNewWallet, replacing the wallet
pointer with nil, and removes the addition of a call to close the
wallet database.  The introduction of a nil pointer caused runtime
panics when executing Loader callbacks that dereferenced the wallet
pointer, and by the caller who would dereference the return value.

This makes the LoaderService.CreateNewWallet RPC useful again.

wallet/loader.go
@@ -150,9 +150,20 @@ func (l *Loader) CreateNewWallet(pubPassphrase, privPassphrase, seed []byte) (*W
 		return nil, err
 	}
 
-	l.onLoaded(nil, db)
-	db.Close()
-	return nil, nil
+	// Open the newly-created wallet.
+	so := l.stakeOptions
+	w, err := Open(db, pubPassphrase, nil, so.VoteBits, so.StakeMiningEnabled,
+		so.BalanceToMaintain, so.AddressReuse, so.RollbackTest,
+		so.PruneTickets, so.TicketAddress, so.TicketMaxPrice,
+		so.TicketBuyFreq, so.PoolAddress, so.PoolFees, l.addrIdxScanLen,
+		l.autoRepair, l.chainParams)
+	if err != nil {
+		return nil, err
+	}
+	w.Start()
+
+	l.onLoaded(w, db)
+	return w, nil
 }
 
 var errNoConsole = errors.New("db upgrade requires console access for additional input")
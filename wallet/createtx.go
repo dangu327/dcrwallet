dangu327/dcrwallet

commit 9fe6e6b6a65516f8ea00c3a4334459251c1cb7a3

multi: update fee estimation for tickets.
This uses EstimateSerializeSizeFromScriptSizes for ticket
fee estimation given its standardized format. Constants
doubleInputTicketSize and singleInputTicketSize are also removed.

wallet/createtx.go
@@ -32,14 +32,6 @@ import (
 // Constants and simple functions
 
 const (
-	// singleInputTicketSize is the typical size of a normal P2PKH ticket
-	// in bytes when the ticket has one input, rounded up.
-	singleInputTicketSize = 300
-
-	// doubleInputTicketSize is the typical size of a normal P2PKH ticket
-	// in bytes when the ticket has two inputs, rounded up.
-	doubleInputTicketSize = 550
-
 	// defaultTicketFeeLimits is the default byte string for the default
 	// fee limits imposed on a ticket.
 	defaultTicketFeeLimits = 0x5800
@@ -943,22 +935,66 @@ func (w *Wallet) purchaseTickets(op errors.Op, req purchaseTicketRequest) ([]*ch
 		return nil, errors.E(op, errors.Invalid, "stakepool fee percent unset")
 	}
 
+	var stakeSubmissionPkScriptSize int
+
+	// The stake submission pkScript is tagged by an OP_SSTX.
+	switch req.ticketAddr.(type) {
+	case *dcrutil.AddressScriptHash:
+		stakeSubmissionPkScriptSize = txsizes.P2SHPkScriptSize + 1
+	case *dcrutil.AddressPubKeyHash:
+		stakeSubmissionPkScriptSize = txsizes.P2PKHPkScriptSize + 1
+	default:
+		return nil, errors.E(op, errors.Invalid,
+			"ticket address must either be P2SH or P2PKH")
+	}
+
 	// Make sure that we have enough funds. Calculate different
 	// ticket required amounts depending on whether or not a
 	// pool output is needed. If the ticket fee increment is
 	// unset in the request, use the global ticket fee increment.
 	var neededPerTicket, ticketFee dcrutil.Amount
+	var estSize int
 	ticketFeeIncrement := req.ticketFee
 	if ticketFeeIncrement == 0 {
 		ticketFeeIncrement = w.TicketFeeIncrement()
 	}
+
+	inSizes := make([]int, 0)
+	outSizes := make([]int, 0)
 	if poolAddress == nil {
-		ticketFee = (ticketFeeIncrement * singleInputTicketSize) / 1000
-		neededPerTicket = ticketFee + ticketPrice
+		// A solo ticket has:
+		//   - a single input redeeming a P2PKH for the worst case size
+		//   - a P2PKH or P2SH stake submission output
+		//   - a ticket commitment output
+		//   - an OP_SSTXCHANGE tagged P2PKH or P2SH change output
+		//
+		//   NB: The wallet currently only supports P2PKH change addresses.
+		//   The network supports both P2PKH and P2SH change addresses however.
+		inSizes = append(inSizes, txsizes.RedeemP2PKHSigScriptSize)
+		outSizes = append(outSizes, stakeSubmissionPkScriptSize,
+			txsizes.TicketCommitmentScriptSize, txsizes.P2PKHPkScriptSize+1)
+		estSize = txsizes.EstimateSerializeSizeFromScriptSizes(inSizes,
+			outSizes, 0)
 	} else {
-		ticketFee = (ticketFeeIncrement * doubleInputTicketSize) / 1000
-		neededPerTicket = ticketFee + ticketPrice
+		// A pool ticket has:
+		//   - two inputs redeeming a P2PKH for the worst case size
+		//   - a P2PKH or P2SH stake submission output
+		//   - two ticket commitment outputs
+		//   - two OP_SSTXCHANGE tagged P2PKH or P2SH change outputs
+		//
+		//   NB: The wallet currently only supports P2PKH change addresses.
+		//   The network supports both P2PKH and P2SH change addresses however.
+		inSizes = append(inSizes, txsizes.RedeemP2PKHSigScriptSize,
+			txsizes.RedeemP2PKHSigScriptSize)
+		outSizes = append(outSizes, stakeSubmissionPkScriptSize,
+			txsizes.TicketCommitmentScriptSize, txsizes.TicketCommitmentScriptSize,
+			txsizes.P2PKHPkScriptSize+1, txsizes.P2PKHPkScriptSize+1)
+		estSize = txsizes.EstimateSerializeSizeFromScriptSizes(inSizes,
+			outSizes, 0)
 	}
+
+	ticketFee = txrules.FeeForSerializeSize(ticketFeeIncrement, estSize)
+	neededPerTicket = ticketFee + ticketPrice
 
 	// If we need to calculate the amount for a pool fee percentage,
 	// do so now.

wallet/internal/txsizes/size.go
@@ -68,6 +68,25 @@ const (
 	//   - OP_CHECKSIG
 	P2PKHPkScriptSize = 1 + 1 + 1 + 20 + 1 + 1
 
+	// P2SHPkScriptSize is the size of a transaction output script that
+	// pays to a script hash.  It is calculated as:
+	//
+	//   - OP_HASH160
+	//   - OP_DATA_20
+	//   - 20 bytes script hash
+	//   - OP_EQUAL
+	P2SHPkScriptSize = 1 + 1 + 20 + 1
+
+	// TicketCommitmentScriptSize is the size of a ticket purchase commitment
+	// script. It is calculated as:
+	//
+	//   - OP_RETURN
+	//   - OP_DATA_30
+	//   - 20 bytes P2SH/P2PKH
+	//   - 8 byte amount
+	//   - 2 byte fee range limits
+	TicketCommitmentScriptSize = 1 + 1 + 20 + 8 + 2
+
 	// P2PKHOutputSize is the serialize size of a transaction output with a
 	// P2PKH output script.  It is calculated as:
 	//

commit 49e8a421be947555c00c4815e87ac04e1e3f9590

Allow empty string purchaseticket voting addresses
When an empty string is used as the voting address in a purchaseticket
JSON-RPC call, the wallet will generate a P2PKH voting address.  This
behavior broke in 9fe6e6b6, and is fixed here by allowing the nil
address in a request.

wallet/createtx.go
@@ -941,7 +941,7 @@ func (w *Wallet) purchaseTickets(op errors.Op, req purchaseTicketRequest) ([]*ch
 	switch req.ticketAddr.(type) {
 	case *dcrutil.AddressScriptHash:
 		stakeSubmissionPkScriptSize = txsizes.P2SHPkScriptSize + 1
-	case *dcrutil.AddressPubKeyHash:
+	case *dcrutil.AddressPubKeyHash, nil:
 		stakeSubmissionPkScriptSize = txsizes.P2PKHPkScriptSize + 1
 	default:
 		return nil, errors.E(op, errors.Invalid,